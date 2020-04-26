1e31f36888613905709a6083767a5ad436d54411
Julio Sena - DTI <juliosouzasena@gmail.com>
2020-04-26 02:31:44 -0300

    feat: create project

---
+++ src/main/java/com/julio/amamenteAVida/AmamenteAVidaApplication.java
@@ -0,0 +1,22 @@
+package com.julio.amamenteAVida;
+
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class AmamenteAVidaApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmamenteAVidaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		
+
+	}
+}
