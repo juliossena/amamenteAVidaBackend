e72f80540d9cba9942e8568ad17c7ec0b13cb82c
Julio Sena - DTI <juliosouzasena@gmail.com>
2020-05-02 14:15:19 -0300

    fix: remove validation email

--- src/main/java/com/julio/amamenteAVida/external/dto/EmailDTO.java
+++ src/main/java/com/julio/amamenteAVida/external/dto/EmailDTO.java
@@ -2,9 +2,6 @@ package com.julio.amamenteAVida.external.dto;
 
 import java.io.Serializable;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotEmpty;
-
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -13,8 +10,6 @@ import lombok.NoArgsConstructor;
 public class EmailDTO implements Serializable {
     private static final long serialVersionUID = -596483918836424367L;
 
-    @NotEmpty(message = "Preenchimento obrigatório")
-    @Email(message = "Email inválido")
     private String email;
 
 
